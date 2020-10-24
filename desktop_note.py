c400426f861f130c1f8937b1f176b24a925a5310
Lumos393 <64517124+Lumos393@users.noreply.github.com>
2020-10-24 18:26:11 -0400

    Create desktop_note.py

---
+++ desktop_note.py
@@ -0,0 +1,15 @@
+import os
+import sys
+import tkinter as tk
+import datetime as dt
+import xml
+
+uinput = input("Enter note now")
+print("e1")
+
+notes = open("notes.xml", "a")
+print("e2")
+notes.write(uinput)
+print("e3")
+
+print(notes.read)
